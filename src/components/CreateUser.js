d2e9fef6d3a6851c55f20123d81969927c027ef7
Terri Bailey <mindsparkle@Mind-Sparkle.local>
2020-01-29 20:40:26 -0500

    more fun

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -2,5 +2,12 @@ import React from "react";
 
 export default function CreateUser({ person }) {
   console.log(person);
-  return <div>test</div>;
+  return <div className="container">
+    <div>
+
+      <button className="btn btn-secondary"></button>
+    </div>
+
+
+  </div>;
 }
