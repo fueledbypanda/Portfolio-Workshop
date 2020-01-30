dc19fdf4a3c76365ca4450aa233c50480056c710
Robert Celestino <panda@Roberts-MacBook-Air.local>
2020-01-29 21:11:49 -0500

    pleeeeaaasee

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -2,12 +2,20 @@ import React from "react";
 
 export default function CreateUser({ person }) {
   console.log(person);
-  return <div className="container">
-    <div>
-
-      <button className="btn btn-secondary"></button>
+  return (
+    <div className="container">
+      <div className="profilePic">
+        <img
+          alt="img"
+          src="https://s3.amazonaws.com/uifaces/faces/twitter/jonathansimmons/128.jpg"
+        />
+      </div>
+      <div className="email">{person.email}</div>
+      <div>
+        <button className="btn btn-primary" onClick={handleClick}>
+          Change User
+        </button>
+      </div>
     </div>
-
-
-  </div>;
+  );
 }
